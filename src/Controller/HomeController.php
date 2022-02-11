1ce65d8007302ff31599bad813f875cde8d0deff
trieu nguyen <trieu181989@gmail.com>
2022-02-10 21:41:13 -0800

    add new base template + homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LoginController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
-     * @Route("/login", name="login")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }

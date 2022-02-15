9941c2571a8be8893081d4b1cd4abf3c3850f4b8
trieu nguyen <trieu181989@gmail.com>
2022-02-15 12:55:26 -0800

    add user admin login

--- src/Security/UserProvider.php
+++ src/Security/UserProvider.php
@@ -1,5 +1,7 @@
 <?php
+
 // src/Security/UserProvider.php
+
 namespace App\Security;
 
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
@@ -7,10 +9,21 @@ use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
 
-class UserProvider implements UserProviderInterface, PasswordUpgraderInterface
-{
-	/**
+class UserProvider implements UserProviderInterface, PasswordUpgraderInterface {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
      * The loadUserByUsername() method was introduced in Symfony 5.3.
      * In previous versions it was called loadUserByUsername()
      *
@@ -20,12 +33,25 @@ class UserProvider implements UserProviderInterface, PasswordUpgraderInterface
      *
      * @throws UserNotFoundException if the user is not found
      */
-    public function loadUserByUsername(string $username): UserInterface
-    {
-        // Load a User object from your data source or throw UserNotFoundException.
-        // The $identifier argument is whatever value is being returned by the
-        // getUserIdentifier() method in your User class.
-        throw new \Exception('TODO: fill in loadUserByIdentifier() inside '.__FILE__);
+    public function loadUserByUsername(string $email): UserInterface {
+        $user = $this->findOneUserBy(['email' => $email]);
+
+        if (!$user) {
+            throw new UsernameNotFoundException(
+                    sprintf(
+                            'User with "%s" email does not exist.',
+                            $email
+                    )
+            );
+        }
+
+        return $user;
+    }
+
+    private function findOneUserBy(array $options): ?User {
+        return $this->entityManager
+                        ->getRepository(User::class)
+                        ->findOneBy($options);
     }
 
     /**
@@ -41,32 +67,33 @@ class UserProvider implements UserProviderInterface, PasswordUpgraderInterface
      *
      * @return UserInterface
      */
-    public function refreshUser(UserInterface $user)
-    {
-        if (!$user instanceof User) {
-            throw new UnsupportedUserException(sprintf('Invalid user class "%s".', get_class($user)));
+    public function refreshUser(UserInterface $user) {
+        assert($user instanceof User);
+
+        if (null === $reloadedUser = $this->findOneUserBy(['id' => $user->getId()])) {
+            throw new UsernameNotFoundException(sprintf(
+                'User with ID "%s" could not be reloaded.',
+                $user->getId()
+            ));
         }
 
-        // Return a User object after making sure its data is "fresh".
-        // Or throw a UserNotFoundException if the user no longer exists.
-        throw new \Exception('TODO: fill in refreshUser() inside '.__FILE__);
+        return $reloadedUser;
     }
 
     /**
      * Tells Symfony to use this provider for this User class.
      */
-    public function supportsClass(string $class)
-    {
+    public function supportsClass(string $class) {
         return User::class === $class || is_subclass_of($class, User::class);
     }
 
     /**
      * Upgrades the encoded password of a user, typically for using a better hash algorithm.
      */
-    public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
-    {
+    public function upgradePassword(UserInterface $user, string $newEncodedPassword): void {
         // TODO: when encoded passwords are in use, this method should:
         // 1. persist the new password in the user storage
         // 2. update the $user object with $user->setPassword($newEncodedPassword);
     }
+
 }

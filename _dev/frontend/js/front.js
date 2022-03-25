4e302a433d64aa676d1b452cd8ef229a7933c564
trieu nguyen <trieu181989@gmail.com>
2022-03-24 19:26:14 -0700

    add datetime picker from html dynamic

--- _dev/frontend/js/front.js
+++ _dev/frontend/js/front.js
@@ -42,6 +42,7 @@ $(document).ready(function () {
                     html +='</div>';
                     html+='</div>';
                     $('#work-calendar').append(html);
+					$('#work-calendar #datepicker').datepicker();
                 // $(data).each(function(index,row){
                 //     let html = '';
                 //     html += '<div class="card-title"';

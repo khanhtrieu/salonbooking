ad087bbbf6b7f23d92075c216bad63eae0f56e7d
trieu nguyen <trieu181989@gmail.com>
2022-04-06 16:00:02 -0700

    add homepage design

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Services;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-class HomeController extends AbstractController
-{
+class HomeController extends AbstractController {
+
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
-    {
+    public function index(ManagerRegistry $doctrine, SessionInterface $session): Response {
+        $userid = $session->get('userid');
+        $loggedIn = false;
+        if ($userid != null) {
+            $loggedIn = true;
+        }
+        $services = $doctrine->getRepository(Services::class)->getListHomepageServiceActivated();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+                    'controller_name' => 'HomeController',
+                    'services' => $services,
+                    'loggedIn' => $loggedIn
         ]);
     }
+
 }

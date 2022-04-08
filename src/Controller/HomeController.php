7c86106c6672585a21619aa008efc90accc8ac20
trieu nguyen <trieu181989@gmail.com>
2022-04-07 23:08:46 -0700

    add function upload image in admin for Service

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Services;
 use Doctrine\Persistence\ManagerRegistry;
@@ -14,13 +15,18 @@ class HomeController extends AbstractController {
     /**
      * @Route("/", name="home")
      */
-    public function index(ManagerRegistry $doctrine, SessionInterface $session): Response {
+    public function index(Request $request, ManagerRegistry $doctrine, SessionInterface $session): Response {
         $userid = $session->get('userid');
         $loggedIn = false;
         if ($userid != null) {
             $loggedIn = true;
         }
         $services = $doctrine->getRepository(Services::class)->getListHomepageServiceActivated();
+        foreach ($services as &$service) {
+            if (!empty($service['Thumbnail'])) {
+                $service['Thumbnail'] = $request->getSchemeAndHttpHost() . DIRECTORY_SEPARATOR . Services::SERVER_PATH_TO_IMAGE_FOLDER . DIRECTORY_SEPARATOR . $service['Thumbnail'];
+            }
+        }
         return $this->render('home/index.html.twig', [
                     'controller_name' => 'HomeController',
                     'services' => $services,
